d93f4adca5c103e135ea01daeb9aebada7cfedce
zmy1997 <1225617844@qq.com>
2020-10-09 15:10:43 +0800

    后台文章管理控制类

--- src/main/java/com/zmy/blog/controller/admin/BackArticleController.java
+++ src/main/java/com/zmy/blog/controller/admin/BackArticleController.java
@@ -1,10 +1,12 @@
 package com.zmy.blog.controller.admin;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author: zengmy
  * @description:
  * @date 2020-10-09
  */
-
+@Controller
 public class BackArticleController {
 }
